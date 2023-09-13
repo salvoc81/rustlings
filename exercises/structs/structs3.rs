9fbd5d16bb6c460f65a871db244d2277b0d8405f
salvocos <salvo981@gmail.com>
2023-09-13 14:21:06 +0900

    complete struct exercises

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -4,8 +4,6 @@
 // Make the code compile and the tests pass!
 // Execute `rustlings hint structs3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 struct Package {
     sender_country: String,
@@ -26,12 +24,17 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        let mut international: bool = false;
+        if self.sender_country != self.recipient_country {
+            international = true;
+        }
+        international
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
-        // Something goes here...
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
+        // Calculate the based on the weight and the cents per gram
+        cents_per_gram * self.weight_in_grams
     }
 }
 
